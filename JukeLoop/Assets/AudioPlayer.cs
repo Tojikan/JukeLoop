4bc43fcb68132d920094310380ad5231fdf7b7bf
Tojikan <andrew.eqian@gmail.com>
2018-07-16 17:36:44 -0400

    JukeLoop

---
+++ JukeLoop/Assets/AudioPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource[] audioPlayers;            //drag a bunch of audio sources here that plays sounds for Beats
+    public AudioSource[] backgroundPlayers;       //drag a bunch of audio sources that plays BG sounds that aren't for Beats          
+    public AudioSet audioSet;                    //drag audioset. Contains the playlist that holds all of the sounds for the current song
+
+
+    /// <summary>Receives a Beat and plays it</summary>
+    /// <param name="beat">The beat data to be read</param>
+    public void ReceiveSound(BeatData beat)
+    {
+        PlaySound(beat.audioPlayer, audioSet.audioClips[beat.audioTrack]);
+    }
+
+
+    //Plays a sound given a source and an audio clip
+    private void PlaySound(int source, AudioClip clip)
+    {
+        if (source >= audioPlayers.Length)
+            source = 0;
+
+        AudioSource audio = audioPlayers[source];
+
+        if (audio.isPlaying)
+            audio.Stop();
+        audio.PlayOneShot(clip);
+    }
+}
---
+++ JukeLoop/Assets/AudioSet.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New AudioSet", menuName = "Audio Set", order = 0)]
+public class AudioSet : ScriptableObject
+{
+    public AudioClip[] audioClips;
+}
