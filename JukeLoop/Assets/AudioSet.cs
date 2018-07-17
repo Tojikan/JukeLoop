984118f3f8e346f339079ea0773087fe374bd6ae
Tojikan <andrew.eqian@gmail.com>
2018-07-16 21:27:14 -0400

    Added grid, beats, beat movement, and reader and BPM

--- JukeLoop/Assets/AudioSet.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New AudioSet", menuName = "Audio Set", order = 0)]
-public class AudioSet : ScriptableObject
-{
-    public AudioClip[] audioClips;
-}
